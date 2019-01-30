0c59d3f503ea9a27fb45f0ed662cc65e00907c7e
Arnold Koay <akoay@jewelintuition.com>
2019-01-30 14:00:40 +0800

    'test1'

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ var bodyParser = require('body-parser');
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('combined'));
+app.use(express.urlencoded());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
